fcf13fbfc3adf40c65dedd0b16c730efcbd92985
Dave Abrahams <dabrahams@adobe.com>
2024-03-08 16:55:20 -0800

    Clean up dependency-fetching.

--- cmake/modules/GenerateSwiftXCTestMain_FetchDependencies.cmake
+++ cmake/modules/GenerateSwiftXCTestMain_FetchDependencies.cmake
@@ -3,31 +3,36 @@
 
 include(FetchContent)
 
-set(saved_FETCHCONTENT_TRY_FIND_PACKAGE_MODE ${FETCHCONTENT_TRY_FIND_PACKAGE_MODE})
-set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE NEVER)
+block()
 
-FetchContent_Declare(SwiftCMakeXCTesting
-  GIT_REPOSITORY https://github.com/hylo-lang/SwiftCMakeXCTesting.git
-  GIT_TAG        main
-  OVERRIDE_FIND_PACKAGE
-)
+  set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE NEVER)
+
+  FetchContent_Declare(SwiftCMakeXCTesting
+    GIT_REPOSITORY https://github.com/hylo-lang/SwiftCMakeXCTesting.git
+    GIT_TAG        main
+    OVERRIDE_FIND_PACKAGE
+  )
+
+  FetchContent_Declare(SwiftSyntax
+    GIT_REPOSITORY https://github.com/apple/swift-syntax.git
+    GIT_TAG        main
+    OVERRIDE_FIND_PACKAGE
+  )
+
+  FetchContent_Declare(ArgumentParser
+    GIT_REPOSITORY https://github.com/apple/swift-argument-parser.git
+    GIT_TAG        1.3.0
+    OVERRIDE_FIND_PACKAGE
+  )
+
+endblock()
+
+# ArgumentParser relies on XCTest even in its non-test code and it seems not to have the necessary
+# target properties to find it, at least in some contexts.  Hopefully FindSwiftXCTest sets those up
+# in time.
 FetchContent_Populate(SwiftCMakeXCTesting)
 list(PREPEND CMAKE_MODULE_PATH ${swiftcmakexctesting_SOURCE_DIR}/cmake/modules)
 include(FindSwiftXCTest)
-
-FetchContent_Declare(SwiftSyntax
-  GIT_REPOSITORY https://github.com/apple/swift-syntax.git
-  GIT_TAG        main
-  OVERRIDE_FIND_PACKAGE
-)
-
-FetchContent_Declare(ArgumentParser
-  GIT_REPOSITORY https://github.com/apple/swift-argument-parser.git
-  GIT_TAG        1.3.0
-  OVERRIDE_FIND_PACKAGE
-)
-
-set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE ${saved_FETCHCONTENT_TRY_FIND_PACKAGE_MODE})
 
 # See https://github.com/apple/swift-syntax/pull/2454, which will hopefully make this clause
 # unnecessary.  Without it CMake will fatal error when loading SwiftSyntax on some platforms.
@@ -37,15 +42,20 @@ if("${SWIFT_HOST_MODULE_TRIPLE}" STREQUAL "")
   string(JSON SWIFT_HOST_MODULE_TRIPLE GET ${target-info} target moduleTriple)
 endif()
 
-set(saved_BUILD_EXAMPLES ${BUILD_EXAMPLES})
-set(saved_BUILD_EXAMPLES ${BUILD_EXAMPLES})
-set(BUILD_EXAMPLES NO)
-set(BUILD_TESTING NO)
+# Not using block() here because FetchContent_MakeAvailable typically causes dependency-specific
+# global variables to be set, and I'm not sure to what extent they may be needed
+if(YES)
+  set(saved_BUILD_EXAMPLES ${BUILD_EXAMPLES})
+  set(saved_BUILD_EXAMPLES ${BUILD_EXAMPLES})
 
-FetchContent_MakeAvailable(ArgumentParser SwiftSyntax)
+  set(BUILD_EXAMPLES NO)
+  set(BUILD_TESTING NO)
 
-set(BUILD_EXAMPLES ${saved_BUILD_EXAMPLES})
-set(BUILD_EXAMPLES ${saved_BUILD_EXAMPLES})
+  FetchContent_MakeAvailable(ArgumentParser SwiftSyntax)
+
+  set(BUILD_EXAMPLES ${saved_BUILD_EXAMPLES})
+  set(BUILD_EXAMPLES ${saved_BUILD_EXAMPLES})
+endif()
 
 # Suppress noisy warnings from dependencies.
 target_compile_options(SwiftCompilerPluginMessageHandling PRIVATE -suppress-warnings)
